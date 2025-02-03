457a3087c50556ce0d1c596cb75cca082b77d697
Amr maher <amrm80233@>
2025-02-03 22:12:57 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,10 @@
+import "bootstrap/dist/css/bootstrap.min.css";
+import "font-awesome/css/font-awesome.min.css";
+import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import "./App.css";
+
+function App() {
+  return <div className="App"></div>;
+}
+
+export default App;
